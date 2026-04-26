65d8688ed11305ea759cc9ccc9280736b6ead688
Daniel Mataczynski <danielxjay@gmail.com>
2026-04-26 10:27:26 +0100

    maintenace mode on

--- src/maintenance.js
+++ src/maintenance.js
@@ -1,5 +1,5 @@
-export const MAINTENANCE_MODE = false;
-export const MAINTENANCE_RETURN_AT_UTC = "";
+export const MAINTENANCE_MODE = true;
+export const MAINTENANCE_RETURN_AT_UTC = "2026-04-26T10:30:00Z";
 
 export const MAINTENANCE_COPY = {
   eyebrow: "Temporarily offline",
